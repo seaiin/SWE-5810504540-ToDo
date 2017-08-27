ac36b6b478e33d107e1252b869fd7a6173466cfc
seaiin <namefinal@gmail.com>
2017-08-27 18:06:22 +0700

    complete ToDoApp tutorial

--- todolist/todos/views.py
+++ todolist/todos/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from ,models import Todo
+from .models import Todo
 
 def index(request):
     todos = Todo.objects.all()[:10]
@@ -9,3 +9,24 @@ def index(request):
         'todos': todos
     }
     return render(request, 'index.html', context)
+
+def details(request, id):
+    todo = Todo.objects.get(id=id)
+
+    context = {
+        'todo': todo
+    }
+
+    return render(request, 'details.html', context)
+
+def add(request):
+    if(request.method == 'POST'):
+        title = request.POST['title']
+        text = request.POST['text']
+
+        todo = Todo(title=title, text=text)
+        todo.save()
+
+        return redirect('/todos')
+    else:
+        return render(request, 'add.html')
